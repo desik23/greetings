b49ae1f50467ac97ab6666207698a06dfd0e4abf
desik23 <desik@bren.ucsb.edu>
2021-08-24 16:32:57 -0700

    updated

--- R/aloha.R
+++ R/aloha.R
@@ -9,20 +9,24 @@
 #' @export
 #'
 #' @examples say_aloha("Allison")
-say_aloha <- function(name, color) {
-
-  message <- paste("Aloha,",
+say_aloha <- function(name, color){
+  message1 <- paste("Aloha,",
                    name,
                    emo::ji("palm_tree"),
                    emo::ji("sunny"),
                    emo::ji("ocean"))
 
-  if(color == "green") {
-    cat(crayon::bgGreen(message))
+  crayon_bgcolor <-c("black", "red", "green", "yellow", "blue", "magenta", "cyan", "white")
+  crayon_bg <- c("bgBlack", "bgRed", "bgGreen", "bgYellow", "bgBlue", "bgMagenta", "bgCyan", "bgWhite")
+
+
+  bgcolor <- data.frame(crayon_bgcolor, crayon_bg)
+
+  for(i in seq_along(crayon_bgcolor)) {
+    if (crayon_bgcolor[i] == color) {
+      parse(cat(paste0("crayon::", bgcolor[i,2], "(", message1)))
+    }
 
   }
-  else if(color == "blue") {
-    cat(crayon::bgBlue(message))
-  }
+
 }
-
